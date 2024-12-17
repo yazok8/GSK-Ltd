3ac85427a86742abd16248dbe2f75fefa5db784f
yazok8 <yazankhirfan@gmail.com>
2024-12-17 10:15:01 -0500

    Empty-Commit

--- src/app/admin/actions/categories.tsx
+++ src/app/admin/actions/categories.tsx
@@ -1,4 +1,5 @@
 // src/app/admin/actions/categories.tsx
+export const dynamic = 'force-dynamic';
 
 import prisma from '@/lib/prisma';
 import { DeleteObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
