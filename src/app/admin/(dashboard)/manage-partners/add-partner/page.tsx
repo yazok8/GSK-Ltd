5d3cda502d0810aa46bbf6dfac615250436523e6
yazok8 <yazankhirfan@gmail.com>
2025-01-15 13:28:23 -0500

    add parners section

--- src/app/admin/(dashboard)/manage-partners/add-partner/page.tsx
+++ src/app/admin/(dashboard)/manage-partners/add-partner/page.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-import PartnerForm from '../_components/PartnerForm'
+import React from "react";
+import PartnerForm from "../_components/PartnerForm";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
-export default function AddPartner() {
-  return (
-    <PartnerForm />
-  )
+export default async function AddPartner() {
+  const session = await getServerSession(authOptions);
+  if (!session || session.user?.role === "VIEW_ONLY") {
+    return (
+      <p className="text-center mt-10">
+        You do not have permission to add or edit products.
+      </p>
+    );
+  }
+  return <PartnerForm />;
 }
